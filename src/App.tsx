9a3d4c74461298c0f3a769e1c53accababd426c7
Jean <jean118877@hotmail.com>
2022-11-25 12:01:32 -0300

    feat: atualização

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,14 @@ import { About } from './componentes/about/About';
 
 function App() { // 2B || !2B
   return (
-    <>
-      <Router> // orquestrador = monta a pagia que o usuário vai ver
-        <Navbar/>
-        <Routes> // Lista de rotas da aplicação
-          <Route path="/" element={<Home/>} /> // Rota
-          <Route path="/home" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-        </Routes>
-      </Router>
-    </>
+    <Router> // orquestrador = monta a pagia que o usuário vai ver
+      <Navbar/>
+      <Routes> // Lista de rotas da aplicação
+        <Route path="/" element={<Home/>} /> // Rota
+        <Route path="/home" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </Router>
   );
 }
 
