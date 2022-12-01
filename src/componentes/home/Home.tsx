7a36d229289e4f28ac50c2b3bd6e2a6a6d0d3d92
Jean <jean118877@hotmail.com>
2022-12-01 12:00:38 -0300

    feat set effect

--- src/componentes/home/Home.tsx
+++ src/componentes/home/Home.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
+import { Button } from '@material-ui/core';
 
 function Home(){
+  const [num, setNum] = useState(0); // [variável, função]
+  useEffect(() => {
+    document.title = `Clicou ${num} vezes`;
+
+    return () => {
+
+    }
+  }, [num]);
+  
   return(
-    <h1>Página Home</h1>
+    <>
+      <h1>Página Home</h1>
+      <h2>você clicou {num} vezes</h2>
+
+      <Button variant="contained" color="primary" onClick={() => setNum(num + 1)}>
+        +
+      </Button>
+      <Button variant="contained" color="primary" onClick={() => setNum(num - 1)}>
+        -
+      </Button>
+    </>
   );
 }
 
